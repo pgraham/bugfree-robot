27aad80739ff142283d31ab056777030a25108bb
Philip Graham <philip.robert.graham@gmail.com>
2013-09-13 16:41:37 -0400

    Improved date parsing and output

--- dates.php
+++ dates.php
@@ -3,10 +3,39 @@ namespace bugfree\robot;
 
 use \DateTime;
 
+$months = [
+	'january', 'jan',
+	'february', 'feb',
+	'march', 'mar',
+	'april', 'apr',
+	'may',
+	'june', 'jun',
+	'july', 'jul',
+	'august', 'aug',
+	'september', 'sep',
+	'october', 'oct',
+	'november', 'nov',
+	'december', 'dec'
+];
+
+$monthsReFrag = '(' . implode('|', $months) . ')';
+$monthRe = "/$monthsReFrag( \d{4})?|\d{4}-\d{2}/";
+
+/**
+ * Return true is the given date string only specifies the month portion of a
+ * date.
+ */
+function isMonth($dateStr) {
+	global $monthRe;
+	return preg_match($monthRe, $dateStr);
+}
+
 function getMonthRange($month = null) {
 
 	if ($month === null) {
 		$month = new DateTime();
+	} else if (is_string($month)) {
+		$month = new DateTime($month);
 	}
 
 	$start = clone $month;
@@ -14,6 +43,25 @@ function getMonthRange($month = null) {
 
 	$start->modify('first day of this month');
 	$end->modify('last day of this month');
+
+	return [ $start, $end ];
+}
+
+function parseDateRange($start, $end) {
+
+	if (isMonth($start)) {
+		$start = new DateTime($start);
+		$start->modify('first day of this month');
+	} else {
+		$start = new DateTime($start);
+	}
+
+	if (isMonth($end)) {
+		$end = new DateTime($end);
+		$end->modify('last day of this month');
+	} else {
+		$end = new DateTime($end);
+	}
 
 	return [ $start, $end ];
 }
--- my-hg-log.php
+++ my-hg-log.php
@@ -8,15 +8,29 @@ require 'props.php';
 
 $props = loadProperties();
 
+/*
+ * =============================================================================
+ * Validate config
+ * =============================================================================
+ */
+
 if (!isset($props['hg']['users'])) {
 	echo "You must specify hg.users in ~/.my-hg-log.yaml\n";
-	exit; }
+	exit;
+}
 $hgUsers = $props['hg']['users'];
 
 if (!isset($props['hg']['repos'])) {
 	echo "You must specify hg.repos in ~/.my-hg-log.yaml\n";
+	exit;
 }
 $hgRepos = $props['hg']['repos'];
+
+/*
+ * =============================================================================
+ * Normalize config
+ * =============================================================================
+ */
 
 if (!is_array($hgUsers)) {
 	$hgUsers = [ $hgUsers ];
@@ -25,24 +39,54 @@ if (!is_array($hgRepos)) {
 	$hgRepos = [ $hgRepos ];
 }
 
+/*
+ * =============================================================================
+ * Parse date argument
+ * =============================================================================
+ */
+
 if ($argc <= 1) {
 	$dates = getMonthRange();
 } else {
-	$dates = implode(' ', array_slice($argv, 1));
+	$date = implode(' ', array_slice($argv, 1));
 
-	if (strpos($dates, ' to ') !== false) {
-		$dates = explode(' to ', $dates);
+	if (strpos($date, ' to ') !== false) {
+		$dates = explode(' to ', $date);
 
-		// TODO
+		$start = $dates[0];
+		$end = $dates[1];
+
+		$dates = parseDateRange($start, $end);
+	} else if (isMonth($date)) {
+		$dates = getMonthRange($date);
+	} else {
+		$dates = new DateTime($date);
 	}
 }
+
+/*
+ * =============================================================================
+ * Build hg command to execute remotely.
+ * =============================================================================
+ */
 
 $hgUsersStr = implode(' ', array_map(function ($user) {
 	return "--user \"$user\"";
 }, $hgUsers));
-$hgDateStr = '--date "' . $dates[0]->format('Y-m-d') . ' to ' . $dates[1]->format('Y-m-d') . '"';
+
+if (is_array($dates)) {
+	$hgDateStr = '--date "' . $dates[0]->format('Y-m-d') . ' to ' . $dates[1]->format('Y-m-d') . '"';
+} else {
+	$hgDateStr = '--date "' . $dates->format('Y-m-d') . '"';
+}
 
 $hgCmd = "hg log $hgUsersStr $hgDateStr";
+
+/*
+ * =============================================================================
+ * Get remote log from each repo.
+ * =============================================================================
+ */
 
 foreach ($hgRepos as $repo) {
 	if (preg_match('/^ssh:\/\/(\w+)@([\w.]+)\/(.+)$/', $repo, $matches)) {
@@ -51,15 +95,20 @@ foreach ($hgRepos as $repo) {
 		$sshPath = $matches[3];
 
 		$sshCmd = "ssh $sshUser@$sshHost 'cd $sshPath; $hgCmd'";
-		echo "Executing $sshCmd\n";
 		unset($sshOutput, $sshReturn);
 		exec($sshCmd, $sshOutput, $sshReturn);
+
+		echo "\n##\n## $repo\n##\n\n";
 
 		if ($sshReturn) {
 			echo "Warning: ssh command exited with status: $sshReturn\n";
 		}
 
-		echo implode("\n", $sshOutput) . "\n";
+		if (!empty($sshOutput)) {
+			echo implode("\n", $sshOutput) . "\n";
+		} else {
+			echo "No commits.\n";
+		}
 
 	} else {
 		echo "Warning: $repo is not a recognized repo URL\n";
